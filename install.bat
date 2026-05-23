166fb6aba58ca8b0cc87fb72cc27c4ed9cde0af6
akkanee-chatanavin <jatanavin@gmail.com>
2026-05-23 21:28:28 +0700

    cleanup dependencies

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 @echo off
 echo Installing VachanaTTS2...
 pip install pythainlp ssg requests
+pip install torch torchaudio --index-url https://download.pytorch.org/whl/cu130
 pip install --pre --index-url https://aiinfra.pkgs.visualstudio.com/PublicPackages/_packaging/ort-cuda-13-nightly/pypi/simple/ onnxruntime-gpu --no-deps
 pip install -e .
 echo Done!
